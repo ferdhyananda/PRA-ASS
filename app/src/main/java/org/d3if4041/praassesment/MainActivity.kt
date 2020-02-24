30bc38ca5e51f85e20d5003e71cd48b87d51e4d2
mferdhy55 <ferdhy889@gmail.com>
2020-02-24 12:06:19 +0700

    praasessment

---
+++ app/src/main/java/org/d3if4041/praassesment/MainActivity.kt
@@ -0,0 +1,13 @@
+package org.d3if4041.praassesment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
